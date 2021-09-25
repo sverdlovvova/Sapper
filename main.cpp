e4034555f898130c0a8978c9fe0865149311eb78
sverdlovvova <83698919+sverdlovvova@users.noreply.github.com>
2021-09-25 13:32:37 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,142 @@
+#include <iostream>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+#include <fstream>
+
+int main() {
+    std::fstream fin("saved_game.txt");
+    std::srand(std::time(nullptr));
+
+    int n, m, k;
+    std::vector<std::vector<int>> field;
+    std::vector<std::vector<int>> opens;
+
+    char yn = 'N';
+    if (fin) {
+        std::cout << "Do you want to continue the previous game? [Y/N]" << std::endl;
+        std::cin >> yn;
+    }
+    if (yn == 'Y') {
+        fin >> n >> m >> k;
+        field.assign(n + 2, std::vector<int> (m + 2, 0));
+        opens.assign(n + 2, std::vector<int> (m + 2, 0));
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= m; ++j) {
+                int x;
+                fin >> x;
+                if ((i + j) % 2 == x % 2) {
+                    field[i][j] = 0;
+                } else {
+                    field[i][j] = 1;
+                }
+            }
+        }
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= m; ++j) {
+                fin >> opens[i][j];
+            }
+        }
+    } else {
+        std::cout << "Enter field size and number of bombs:" << std::endl;
+        std::cin >> n >> m >> k;
+        while (k >= n * m / 2) {
+            std::cout << "Too many bombs. Number of bombs must be less than half of number of cells. Choose different. Enter field size and a number of bombs again:" << std::endl;
+            std::cin >> n >> m >> k;
+        }
+        field.assign(n + 2, std::vector<int> (m + 2, 0));
+        opens.assign(n + 2, std::vector<int> (m + 2, 0));
+        for (int i = 0; i < n; ++i) {
+            field[i][0] = field[i][m + 1] = field[0][i] = field[n + 1][i] = 1;
+        }
+        k = (std::rand() % 4) + 2;
+        while (k > 0) {
+            int x = std::rand() % 5 + 1;
+            int y = std::rand() % 5;
+            if (field[x][y]) {
+                continue;
+            }
+            if (field[x - 1][y - 1] * field[x - 1][y] * field[x - 1][y + 1] * field[x][y + 1] * field[x + 1][y + 1] * field[x + 1][y] * field[x + 1][y - 1] * field[x][y - 1] == 0) {
+                field[x][y] = 1;
+                k--;
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            field[i][0] = field[i][m + 1] = field[0][i] = field[n + 1][i] = 0;
+        }
+    }
+
+    fin.close();
+    fin.clear();
+
+    while (true) {
+        std::cout << "Enter command:" << std::endl;
+        std::string s;
+        std::cin >> s;
+        int x, y;
+        std::string action;
+        if ('0' <= s[0] && s[0] <= '9') {
+            x = stoi(s);
+            std::cin >> y >> action;
+        } else {
+            fin << n << m << k << std::endl;
+            for (int i = 1; i <= n; ++i) {
+                for (int j = 1; j <= n; ++j) {
+                    fin << field[i][j] << ' ';
+                }
+                fin << std::endl;
+            }
+            for (int i = 1; i <= n; ++i) {
+                for (int j = 1; j <= n; ++j) {
+                    fin << (opens[i][j] + i + j) % 2 << ' ';
+                }
+                fin << std::endl;
+            }
+            break;
+        }
+        if (x < 1 || x > n || y < 1 || y > m) {
+            std::cout << "Wrong answer. Choose a cell within the bounds of the field" << std::endl;
+        }
+        if (action == "Flag") {
+            if (opens[x][y] == 1) {
+                std::cout << "Wrong command: the cell is already open" << std::endl;
+            } else {
+                std::cout << "Ok" << std::endl;
+            }
+            if (opens[x][y] == 0) {
+                opens[x][y] = 2;
+            }
+        }
+        if (action == "Open") {
+            if (field[x][y]) {
+                std::cout << "You lose" << std::endl;
+                break;
+            } else {
+                opens[x][y] = 1;
+                std::cout << "Ok" << std::endl;
+            }
+        }
+        int cnt_not_open = 0, cnt_bombs = 0;
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= m; ++j) {
+                if (opens[x][y] == 0) {
+                    std::cout << "*";
+                    cnt_not_open++;
+                } else if (opens[x][y] == 1) {
+                    std::cout << field[x - 1][y - 1] + field[x - 1][y] + field[x - 1][y + 1] + field[x][y + 1] +
+                    field[x + 1][y + 1] + field[x + 1][y] + field[x + 1][y - 1] + field[x][y - 1];
+                } else if (opens[x][y] == 2) {
+                    std::cout << "F";
+                    cnt_bombs++;
+                }
+            }
+            std::cout << std::endl;
+        }
+        if (cnt_not_open == 0 && cnt_bombs == k) {
+            std::cout << "You win" << std::endl;
+            fin.clear();
+            break;
+        }
+    }
+}
+
